b37c6ccb8f4eb3f7245863aa2177f5ee26512b2e
TheNootLinja <thenootlinja@gmail.com>
2023-07-21 11:27:31 -0400

    Add initial skeleton of app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import gameArr from './data/game-list'
+import { useState } from 'react';
 
 function App() {
+
+  const [collectedGameArr, setCollectedGameArr] = useState([])
+
+  // TODO: When game is clicked, add it to the collectedGameArr
+  const addGameToCollection = (id) => {
+    const newGameCollectionArr = [...collectedGameArr, id];
+    setCollectedGameArr(newGameCollectionArr)
+  }
+
+  // setCollectedGameArr([1,2,3,4,5])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Get the Set</h1>
+      <h2>{gameArr.filter(game => !collectedGameArr.includes(game.id)).length} Games Remaining!</h2>
+      <div className="card-container">
+      {
+      gameArr.map((game) => {
+        return (
+        <div className="card" onClick={() => addGameToCollection(game.id)}>
+          <p>{game.title}</p>
+          <p>{game.id}</p>
+        </div>
+      )
+      })
+      }
+      </div>
     </div>
   );
 }
---
+++ src/data/game-list.js
@@ -0,0 +1,412 @@
+const gameArr = [
+    {
+        title: "1-2 Switch",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 1
+    },
+    {
+        title: "Advance Wars 1 + 2: Re-Boot Camp",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 2
+    },
+    {
+        title: "Animal Crossing: New Horizons",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 3
+    },
+    {
+        title: "Arms",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 4
+    },
+    {
+        title: "Astral Chain",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 5
+    },
+    {
+        title: "Bayonetta",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 6
+    },
+    {
+        title: "Bayonetta 2",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 7
+    },
+    {
+        title: "Bayonetta 3",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 8
+    },
+    {
+        title: "Bayonetta Origins: Cereza and the Lost Demon",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 9
+    },
+    {
+        title: "Big Brain Academy: Brain vs. Brain",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 10
+    },
+    {
+        title: "Bravely Default II",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 11
+    },
+    {
+        title: "Cadence of Hyrule: Crypt of the Necrodancer Featuring The Legend of Zelda",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 12
+    },
+    {
+        title: "Captain Toad: Treasure Tracker",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 13
+    },
+    {
+        title: "Clubhouse Games: 51 Worldwide Classics",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 14
+    },
+    {
+        title: "Daemon X Machina",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 15
+    },
+    {
+        title: "DC Super Hero Girls: Teen Power",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 16
+    },
+    {
+        title: "Donkey Kong Country: Tropical Freeze",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 17
+    },
+    {
+        title: "Dr Kawashima's Brain Training for Nintendo Switch",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 18
+    },
+    {
+        title: "Dragon Quest Builders",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 19
+    },
+    {
+        title: "Dragon Quest XI S: Echoes of an Elusive Age - Definitive Edition",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 20
+    },
+    {
+        title: "Fire Emblem (30th Anniversary Edition)",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 21
+    },
+    {
+        title: "Fire Emblem Engage",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 22
+    },
+    {
+        title: "Fire Emblem Warriors",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 23
+    },
+    {
+        title: "Fire Emblem Warriors: Three Hopes",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 24
+    },
+    {
+        title: "Fire Emblem: Three Houses",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 25
+    },
+    {
+        title: "Fitness Boxing",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 26
+    },
+    {
+        title: "Fitness Boxing 2: Rhythm & Exercise",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 27
+    },
+    {
+        title: "Game Builder Garage",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 28
+    },
+    {
+        title: "Go Vacation",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 29
+    },
+    {
+        title: "Hyrule Warriors: Age of Calamity",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 30
+    },
+    {
+        title: "Hyrule Warriors: Definitive Edition",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 31
+    },
+    {
+        title: "Kirby and the Forgotten Land",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 32
+    },
+    {
+        title: "Kirby Star Allies",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 33
+    },
+    {
+        title: "Kirby's Return to Dream Land Deluxe",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 34
+    },
+    {
+        title: "Layton's Mystery Journey: Katrielle and The Millionaires' Conspiracy - Deluxe Edition",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 35
+    },
+    {
+        title: "The Legend of Zelda: Breath of the Wild",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 36
+    },
+    {
+        title: "The Legend of Zelda: Links Awakening",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 37
+    },
+    {
+        title: "The Legend of Zelda: Skyward Sword HD",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 38
+    },
+    {
+        title: "The Legend of Zelda: Tears of the Kingdom",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 39
+    },
+    {
+        title: "Luigi's Mansion 3",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 40
+    },
+    {
+        title: "Mario Golf: Super Rush",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 41
+    },
+    {
+        title: "Mario Kart 8 Deluxe",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 42
+    },
+    {
+        title: "Mario Kart Live: Home Circuit (Luigi Set)",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 43
+    },
+    {
+        title: "Mario Kart Live: Home Circuit (Mario Set)",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 44
+    },
+    {
+        title: "Mario Party Superstars",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 45
+    },
+    {
+        title: "Mario Strikers: Battle League",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 46
+    },
+    {
+        title: "Mario Tennis Aces",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 47
+    },
+    {
+        title: "Marvel Ultimate Alliance 3: The Black Order",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 48
+    },
+    {
+        title: "Metroid Dread",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 49
+    },
+    {
+        title: "Metroid Prime Remastered",
+        publisher: "Nintendo",
+        releaseDate: "",
+        genre: "",
+        coverImage: "",
+        id: 50
+    },
+    // {
+    //     title: "",
+    //     publisher: "Nintendo",
+    //     releaseDate: "",
+    //     genre: "",
+    //     coverImage: "",
+    //     id: 4
+    // },
+]
+
+export default gameArr;
